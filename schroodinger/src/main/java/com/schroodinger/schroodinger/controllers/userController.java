b13f2ee7fc4f9244fa4ebae6b780d554fa3e0d12
makgalemela <schroodinger@gmail.com>
2020-03-14 15:44:51 +0200

    First commit

--- schroodinger/src/main/java/com/schroodinger/schroodinger/controllers/userController.java
+++ schroodinger/src/main/java/com/schroodinger/schroodinger/controllers/userController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,5 +25,11 @@ public class userController {
   public ResponseEntity getUsers( ){
     List<user>  allUser = _userService.getAllUsers();
    return  new ResponseEntity(allUser , HttpStatus.OK);
+  }
+
+  @RequestMapping(method = RequestMethod.POST , value = "/saveUser")
+  public ResponseEntity saveUser(@RequestBody user _user){
+      user __user = _userService.createUser(_user);
+      return new ResponseEntity(__user , HttpStatus.OK);
   }
 }
